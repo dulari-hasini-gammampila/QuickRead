f0c30e1d1bb2732f3ee14537f03d9337f8f3c7a1
dularihasinigammampila <dularihasini18@gmail.com>
2025-04-13 18:47:12 +0800

    - Created library display with current books - Included recommendations button"

--- app/src/main/java/com/example/quickread/screens/HomeScreen.kt
+++ app/src/main/java/com/example/quickread/screens/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.example.quickread.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import com.example.quickread.data.BookRepository
-import com.example.quickread.models.Book
+import com.example.quickread.data.Book
+
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    val books = BookRepository.books  // Use the centralized book list
+    val books = BookRepository.getBooks()  // Use the centralized book list
 
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Text("My Library", style = MaterialTheme.typography.headlineLarge)
